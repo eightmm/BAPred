9fcda06f6824efaa12e8c0c0cfdf14633f6b10c1
eightmm <sim0767@naver.com>
2025-10-21 15:13:49 +0900

    sanitize

--- example/run.sh
+++ example/run.sh
@@ -2,4 +2,4 @@
 python_path="python"
 inference_py="../run_inference.py"
 
-$python_path $inference_py -r ./1KLT.pdb -l ligands.sdf -o result.tsv --model_path '/home/jaemin/git/BAPred/bapred/weight/'
+$python_path $inference_py -r ./1KLT.pdb -l ligands.sdf -o result.tsv --model_path '../bapred/weight/'
